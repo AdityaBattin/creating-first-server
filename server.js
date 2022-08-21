b9fba1e0796044b1cbeccb10ca8f0261efbeeb07
AdityaBattin <aditya.g.battin@gmail.com>
2022-08-21 18:41:22 +0530

    understood the code and installed dependences

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const http = require("http");
 const port = 8081;
 
 http
-  .createServer((request, responce) => {
-    responce.writeHead(200, { "Content-Type": "text/html" });
-    responce.write("<h1>Hello, this is from my server</h1>");
-    responce.end();
+  .createServer((req, res) => {
+    //call back function for request responce
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Hello, this is from my server :)</h1>");
+    res.end();
   })
   .listen(port, () => {
+    //call back fuction
     console.log(`Nodejs server stated on port ${port}`);
   });
 
